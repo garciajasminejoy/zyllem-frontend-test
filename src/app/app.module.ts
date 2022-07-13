88dcd21f6b75f58253ff402fa014c1d0342d9d1f
Jasmine Garcia <garciajasminejoy@gmail.com>
2022-07-13 17:10:49 +0800

    initialize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ZyllemApiService } from './app.service';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { FeaturedArticleComponent } from './components/articles/featured/featured-article.component';
+import { NormalArticleComponent } from './components/articles/normal/normal-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturedArticleComponent,
+    NormalArticleComponent,
+    ArticlesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [ZyllemApiService],
   bootstrap: [AppComponent]
